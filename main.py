f6c8a8b0708ce7cdd10bc28e4cc3628197540bc6
thomasBontemps <thomas.bontemps@isen-ouest.yncrea.fr>
2022-03-23 16:09:58 +0100

    Enfin fini ? multiflot valeur

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ premiereSolution = 0
 for l in links:
     premiereSolution += l.getSUCtotal()
     #print("src = ", l.getInput(), "\tOutput = ", l.getOutput(), "\t suctotal = ",l.getSUCtotal())
-    #l.reinitialisationSUCtotal(g)
+    l.reinitialisationSUCtotal(g)
 
 print("Le coût global de la solution est de ", round(premiereSolution, 2))
 
@@ -32,8 +32,12 @@ flotProblem = multiFlots(g, links, demands)
 i = 0
 sommmeTotal = 0
 for fp in flotProblem:
-    sommmeTotal += getVal(links[i].getSUC(), fp.value())
+    links[i].setPIC(fp.value())
+    setPriceSucTotal(links[i])
+    sommmeTotal += links[i].getSUCtotal()
     i += 1
+
+showGraph(g, nodes, links)
 
 print("Avec la méthode de Pulp, nous obtenons un SUC total de :",sommmeTotal)
 
--- utils/multiFlots.py
+++ utils/multiFlots.py
@@ -6,13 +6,12 @@ from utils.showGraph import showGraph
 
 
 def multiFlots(graphe, links, demands):
-
     lengthLinks = len(links)
     rangeLinks = range(0, lengthLinks)
 
     capaciteList = [i for i in rangeLinks]
 
-    X = pulp.LpVariable.dicts("x", capaciteList, lowBound=0, cat="Continuous")
+    X = pulp.LpVariable.dicts("X", capaciteList, lowBound=0, cat="Continuous")
 
     flotProblem = pulp.LpProblem("MultiFlots", pulp.LpMinimize)
 
@@ -25,61 +24,60 @@ def multiFlots(graphe, links, demands):
 
     # Definition of the constraints
 
-
     N = []
     flux = []
     for demand in demands:
         # Calculate k-shortest path
         N_prime = np.zeros(lengthLinks)
+        N_prime_bis = []
         paths = yenKSP(graphe, demand.getInput(), demand.getOutput())
         for path in paths:
-            rangePath = range(0,len(path)-1)
+            rangePath = range(0, len(path) - 1)
             for i in rangePath:
                 for link in links:
                     if (link.getInput() == path[i] or link.getInput() == path[i + 1]) and (
                             link.getOutput() == path[i] or link.getOutput() == path[i + 1]):
-                        N_prime[links.index(link)] += 1
+                        N_prime[links.index(link)] += 1  # Determine how many times branch it uses for this demand
+                        N_prime_bis += X[links.index(link)]
         N.append(N_prime)
+
+        # Create a list that contain each variable uses multiply by n time
         listX = []
         rangeN_prime = range(0, len(N_prime))
         for i in rangeN_prime:
-            multiplicity = N_prime[i]
-            if multiplicity > 0:
-                listX.append(multiplicity * X[i])
+            listX.append(N_prime[i] * X[i])
 
-        # Contrainte 1 : sum(pi) >= flux
+        # Contrainte 1 : sum(pi) >= flux ; The sum of all path for this demand should contain flow of this demand
         flotProblem += pulp.lpSum(listX) >= demand.getFlux(), "demande_" + str(demands.index(demand))
 
         flux.append(demand.getFlux())
 
-    # Contrainte 2 : sum(capacité d'une arrete) <= sum(flux totaux passant par cette arrête)
-    listCapaciteFlux = [ [X[i], 0] for i in rangeLinks]
-
+    # Finds max flow of a weight
+    listCapaciteFlux = [[X[i], 0, np.inf] for i in rangeLinks]
     rangeN = range(0, len(N))
-
     for i in rangeN:
         for j in rangeLinks:
+            # If multiplication superior of 0 --> fluxMax = fluxPrecedent += fluxOfDemandI
             if N[i][j] > 0:
                 listCapaciteFlux[j][1] += flux[i]
+            if listCapaciteFlux[j][2] > flux[i]:
+                listCapaciteFlux[j][2] = flux[i]
 
     for tupleCapaciteFlux in listCapaciteFlux:
         print(str(tupleCapaciteFlux[0]))
+        # Contrainte 2 : sum(capacité d'une arête) <= sum(flux totaux passant par cette arête)
         flotProblem += tupleCapaciteFlux[0] <= tupleCapaciteFlux[1], "capaciteMax_" + str(tupleCapaciteFlux[0])
 
-    # Contrainte 3 : Il doit il y avoir autant de capacité que de flux
+        # Contrainte : une arrete >= fluxMinimum
+        flotProblem += tupleCapaciteFlux[0] >= tupleCapaciteFlux[2], "capaciteMinimum_" + str(tupleCapaciteFlux[0])
+
+    # Contrainte 3 : Ensemble des poids doit être égale au flux de chaque demand
     flotProblem += pulp.lpSum(X) == sum(flux), "Flux"
 
+
+
+    # Solve the problem
     flotProblem.solve()
-    #print("Status : ", pulp.LpStatus[flotProblem.status])
 
-    # OUTPUT
-    #print("Solution flot :")
-    demande = "demande: "
-    #print(X)
-    for x in X.values():
-        demande += str(x.value()) + ", "
-    #print(demande)
-
+    # return variable calculated by the system
     return X.values()
-
-
